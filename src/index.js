73f6ffc8760f1101dd12d47dcd7ffdcb2809e459
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2021-10-26 19:52:33 +0300

    Верстка блока промо и блока состав

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import $ from "jquery";
+function setVideoCenter() {
+  var $box = $(".video-box");
+  var height = $box.height();
+  var width = $box.width();
+  var new_height = width / 1.78;
+  if (new_height > height) {
+    $box.find("video").css({
+      width: width,
+      height: new_height,
+      top: "-" + (new_height / 2 - height / 2) + "px",
+      left: "0",
+    });
+  } else {
+    var new_width = height * 1.78;
+    $box.find("video").css({
+      width: new_width,
+      height: height,
+      top: "0",
+      left: "-" + (new_width / 2 - width / 2) + "px",
+    });
+  }
+}
+
+$(function () {
+  setVideoCenter();
+  $(window).resize(setVideoCenter);
+});
