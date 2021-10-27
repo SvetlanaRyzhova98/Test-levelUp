a20476bdf1e501e3b0b6d2b864720b5a337cd193
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2021-10-27 17:50:16 +0300

    Окончание верстки, добавлена анимация, таймер

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import $ from "jquery";
 import "slick-carousel";
+import tippy from "tippy.js";
 
 function setVideoCenter() {
-  var $box = $(".video-box");
-  var height = $box.height();
-  var width = $box.width();
-  var new_height = width / 1.78;
+  let $box = $(".video-box");
+  let height = $box.height();
+  let width = $box.width();
+  let new_height = width / 1.78;
   if (new_height > height) {
     $box.find("video").css({
       width: width,
@@ -14,7 +15,7 @@ function setVideoCenter() {
       left: "0",
     });
   } else {
-    var new_width = height * 1.78;
+    let new_width = height * 1.78;
     $box.find("video").css({
       width: new_width,
       height: height,
@@ -28,10 +29,45 @@ $(function () {
   setVideoCenter();
   $(window).resize(setVideoCenter);
 });
-
+//карусель
 $(".reviews__sliders").slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: true,
+});
+
+// таймер
+const MIN_30 = 30 * 60;
+let startTimeSec = MIN_30;
+setInterval(() => {
+  --startTimeSec;
+  if (startTimeSec < 0) {
+    startTimeSec = MIN_30;
+  }
+
+  const min = Math.floor(startTimeSec / 60);
+  const ostatok = startTimeSec % 60;
+  const sec = ostatok < 10 ? "0" + ostatok : ostatok;
+  $("#getting-started").text(min + ":" + sec);
+}, 1000);
+
+$(".scroll-button").on("click", function () {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $(".order").offset().top,
+    },
+    2000
+  );
+});
+//подсказки
+tippy(".name-input", {
+  content: "Пример: Анастасия Киселева",
+  trigger: "click",
+  placement: "top-end",
+});
+tippy(".phone-input", {
+  content: "Пример: +7 999 888 77 66",
+  trigger: "click",
+  placement: "top-end",
 });
